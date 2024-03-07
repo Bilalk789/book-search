ea05f2e041d081ad0b706865b9dd0bd21250675c
Bilal <bilalk6789@yahoo.com>
2024-03-07 16:52:01 -0600

    save

--- server/models/Book.js
+++ server/models/Book.js
@@ -1,31 +1,14 @@
-const { Schema } = require('mongoose');
+const { gql } = require('apollo-server-express');
 
-// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
-const bookSchema = new Schema({
-  authors: [
-    {
-      type: String,
-    },
-  ],
-  description: {
-    type: String,
-    required: true,
-  },
-  // saved book id from GoogleBooks
-  bookId: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-  link: {
-    type: String,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-});
+const typeDefs = gql`
+  type Book {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+`;
 
-module.exports = bookSchema;
+module.exports = typeDefs;
--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,31 @@
-const User = require('./User');
+const { gql } = require('apollo-server-express');
 
-module.exports = { User };
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    savedBooks: [Book]
+  }
+
+  type Book {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Query {
+    getUser(id: ID!): User
+    getUsers: [User]
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!): User
+    # Add other mutations here, like updateUser and deleteUser
+  }
+`;
+
+module.exports = typeDefs;
